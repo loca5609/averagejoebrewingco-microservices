41b9f181b21a3fd4ebff4ffccadd076aec6890b5
ああああああああ\Yayoi <no>
2018-11-24 12:43:47 -0800

    removed /coffee mapping, so URI is {endpoint}/allCoffee instead of {endpoint}/coffee/allCoffee

--- src/main/java/edu/averagejoecoffeeco/coffeedb/api/CoffeeController.java
+++ src/main/java/edu/averagejoecoffeeco/coffeedb/api/CoffeeController.java
@@ -29,7 +29,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
-@RequestMapping("/coffee")
 public class CoffeeController {
     private static final Logger logger = LoggerFactory.getLogger(CoffeedbApplication.class);
     @Autowired
