90fba99d3f0f1c6920bf46bdf360be57abca3dd5
Jeremy Locatelli <31291511+loca5609@users.noreply.github.com>
2018-12-07 19:58:07 -0800

    Update CoffeeDbSeeder.java
    fixed image URL

--- src/main/java/edu/averagejoecoffeeco/coffeedb/CoffeeDbSeeder.java
+++ src/main/java/edu/averagejoecoffeeco/coffeedb/CoffeeDbSeeder.java
@@ -38,7 +38,7 @@ public class CoffeeDbSeeder implements CommandLineRunner {
         Coffee lastMinute1 = new Coffee("Light Roast","Light Roast","Nutty","Low","Full","Slightly Sweet","USA","Good ol Farms","Organic",
 		"https://banner2.kisspng.com/20180130/ajq/kisspng-coffee-bean-gunny-sack-bag-kopi-luwak-bags-of-coffee-beans-close-up-5a703364186eb0.8779660015173026281001.jpg",100,3.99);
 	    
-	Coffee lastMinute2 = new Coffee("Brutal","Blacker than the Blackest Black times Infinity Roast","Cursed","VERY HIGH","NONE","BITTER","FROM THE HILLS OF COLOMBIA","Duncan Hills Coffee","BRUTAL","https://d1ielco78gv5pf.cloudfront.net/assets/clear-495a83e08fc8e5d7569efe6339a1228ee08292fa1f2bee8e0be6532990cb3852.gif",666,6.66);
+	Coffee lastMinute2 = new Coffee("Brutal","Blacker than the Blackest Black times Infinity Roast","Cursed","VERY HIGH","NONE","BITTER","FROM THE HILLS OF COLOMBIA","Duncan Hills Coffee","BRUTAL","https://ih0.redbubble.net/image.472463630.5051/flat,1000x1000,075,f.u1.jpg",666,6.66);
 	    
 	Coffee why = new Coffee("Good Coffee","Good Coffee","Sweet","Medium","Light","Good?","Colombia","Great Farms","Organic",
 		"https://banner2.kisspng.com/20180130/ajq/kisspng-coffee-bean-gunny-sack-bag-kopi-luwak-bags-of-coffee-beans-close-up-5a703364186eb0.8779660015173026281001.jpg",25,4.99);
